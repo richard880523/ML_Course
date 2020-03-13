6f5ff9fbd48dd52b3cb7442825ec9e01c5c5f1b7
richard880523 <41483965+richard880523@users.noreply.github.com>
2020-03-14 01:26:23 +0800

    Add more statements to solve the problem.

--- ml_play_template.py
+++ ml_play_template.py
@@ -48,8 +48,51 @@ def ml_loop():
         if not ball_served:
             comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_LEFT)
             ball_served = True
+
+            x_prev = scene_info.ball[0]
+            y_prev = scene_info.ball[1]
+            
+            x_update = 80
+
         else:
-            if(scene_info.ball[0] < 80):
-                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+            x_curr = scene_info.ball[0]
+            y_curr = scene_info.ball[1]
+
+            if(y_curr < 120 or y_curr - y_prev < 0): # do not move when ball is up there breaking bricks.
+                comm.send_instruction(scene_info.frame, PlatformAction.NONE)
             else:
-                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
+                if(x_curr == 0):
+                    y_hit_point = y_curr
+                    if(y_hit_point < 200): # will hit again on the other side
+                        y_hit_point += 200
+                        x_update = 200 - (400 - y_hit_point) - (40 / 2)
+                    else:    
+                        x_update = (400 - y_hit_point) - (40 / 2)
+                    # print(x_update)
+
+                elif(x_curr >= 195 and x_curr < 200):
+                    y_hit_point = y_curr
+                    if(y_hit_point < 200):
+                        y_hit_point += 200
+                        x_update = (400 - y_hit_point) - (40 / 2)
+                    else:    
+                        x_update = 200 - (400 - y_hit_point) - (40 / 2) # bias
+                    # print(x_update)
+
+                if(x_update < 0):
+                    x_update = abs(x_update)
+                elif(x_update > 195):
+                    x_update -= 195
+
+                while(x_update % 5 != 0):
+                    x_update += 1 # for movimg plateform smoothly 
+
+                if(scene_info.platform[0] > x_update):
+                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+                elif(scene_info.platform[0] < x_update):
+                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)  
+                else:
+                    comm.send_instruction(scene_info.frame, PlatformAction.NONE)
+
+            x_prev = x_curr
+            y_prev = y_curr        
